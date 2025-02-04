bc89a3dabf67295a4a062ae77639f6475bed44cf
Sapin07 <sapinleste@outlook.com>
2025-02-04 17:13:05 -0300

    implementa function para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const friendList = document.querySelector('#listaAmigos');
+const iptFriends = document.querySelector('#amigo');
+
+
+let friends = []; //lista dos participantes
+
+function updateList(){
+    friendList.innerHTML = " ";
+    
+    for (friend of friends) {
+        const friendItem = friendList.appendChild(document.createElement("li"));
+        friendItem.className = "friend-item";
+        friendItem.id = friend;
+        friendItem.innerHTML = friend ;
+    }
+    console.log(friends);
+}
+
+function adicionarAmigo() {
+    const friendName = iptFriends.value.trim();
+
+    if (friendName === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+    if (friends.includes(friendName)) {
+        alert("Esse amigo já foi adicionado!");
+        return;
+    }
+
+    friends.push(friendName);
+    updateList();
+    iptFriends.value = "";
+}
